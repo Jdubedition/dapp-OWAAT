6e36e6808acd2bed09a171f08e7c0cc5f060eb70
Justin Wesley <justin.t.wesley@gmail.com>
2021-11-12 08:08:22 -0700

    forgot about deploy_contracts.js during previous cleanup

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var RandomNumberGenerator = artifacts.require("./RandomNumberGenerator.sol");
 var Phrase = artifacts.require("./Phrase.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(RandomNumberGenerator);
   deployer.deploy(Phrase);
 };
